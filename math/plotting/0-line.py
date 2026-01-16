8babb5e7683987bcd6b66ca7e7742c8e8020ab76
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-01-16 22:11:32 +0200

    file 0-line.py

---
+++ math/plotting/0-line.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import numpy as np                                                                                                                                           
+import matplotlib.pyplot as plt                                                                                                                              
+                                                                                                                                                             
+x = np.arange(0, 11)                                                                                                                                         
+y = np.arange(0, 11) ** 3                                                                                                                                    
+
+plt.figure(figsize=(10, 6))                                                                                                                                                   
+plt.plot(x, y) 
+plt.show() 
