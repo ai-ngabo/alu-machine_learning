2deb5bf5bbc2bf0ad2895b4a37bad85122f56761
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:41:53 +0200

    12-bracin_the_elements.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -5,6 +5,5 @@ function np_shape that calculates the shape .
 
 
 def np_shape(matrix):
-    """Return the shape of a numpy.ndarray 
-    as a tuple of integers."""
+    """Return the shape of tuple of integers."""
     return matrix.shape
---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines a function that performs operations."""
+
+
+def np_elementwise(mat1, mat2):
+    """addition, subtraction, multiplication, and division."""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
