8e499780b96e11add770c55fcd3734bef360f638
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 22:57:08 +0200

    5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -8,4 +8,7 @@ def add_matrices2D(mat1, mat2):
     """new matrix with sum of mat1 and mat2, or None if shapes differ."""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    return [
+        [mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+        for i in range(len(mat1))
+    ]
