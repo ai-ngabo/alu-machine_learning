a4ac049e4971929639a29baf78596e611858d732
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 22:47:31 +0200

    4-line_up.py

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -5,7 +5,7 @@ Defines a function add_arrays that adds two arrays element-wise.
 
 
 def add_arrays(arr1, arr2):
-    """Return a new list with element-wise sum of arr1 and arr2, or None if shapes differ."""
+    """new list with sum of arr1 and arr2, or None if shapes differ."""
     if len(arr1) != len(arr2):
         return None
     return [arr1[i] + arr2[i] for i in range(len(arr1))]
