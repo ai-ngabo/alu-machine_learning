055fef46f64d61d12e05b6fc434b2f9db2b0db37
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:59:31 +0200

    update

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-Defines a function np_cat that concatenates two numpy.ndarrays along a specific axis.
+Defines a function np_cats.
 """
 
 
 import numpy as np
+
 
 def np_cat(mat1, mat2, axis=0):
     """Concatenate two numpy.ndarrays along the given axis."""
