6c0c861460d82246504aa37da43719009c9ec073
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 22:16:58 +0200

    2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,7 @@
+def matrix_shape(matrix):
+    """Calculate the shape of a matrix as a list of integers."""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if matrix else []
+    return shape
