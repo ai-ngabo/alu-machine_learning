c317c83581c08e800d7c11185b2bed01e364d1bf
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:01:21 +0200

    6-howdy_partner.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Defines a function cat_arrays that concatenates two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """new list that is the concat of arr1 and arr2."""
+    return arr1 + arr2
