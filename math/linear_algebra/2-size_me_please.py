3ac08ad1315edee2cd40fff0f040e52c99811ff3
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 22:24:05 +0200

    2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module for calculating the shape of a matrix."""
 def matrix_shape(matrix):
     """Calculate the shape of a matrix as a list of integers."""
     shape = []
