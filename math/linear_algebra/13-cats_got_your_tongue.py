2862f8413c80cac5d89f2fc7413a8585a2dc730b
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:46:44 +0200

    13-cats_got_your_tongue.py

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""np_cat that concatenates two along a specific axis."""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Concatenate two numpy.ndarrays along the given axis."""
+    return np.concatenate((mat1, mat2), axis=axis)
