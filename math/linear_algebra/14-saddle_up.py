94b5a13df4785dbc630d5a97e90efb59be531ae7
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:58:35 +0200

    update

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -6,6 +6,7 @@ Defines a function np_matmul that performs matrix multiplication using numpy.
 
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
     """Return the matrix product of two numpy.ndarrays."""
     return np.matmul(mat1, mat2)
