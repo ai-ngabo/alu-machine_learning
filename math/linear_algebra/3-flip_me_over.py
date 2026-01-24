996c2d57797cd0de1a56d750d597aa1752adc755
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 22:32:39 +0200

    3-flip_me_over.py

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Defines a function matrix_transpose that returns the transpose of a 2D matrix.
+"""
+
+def matrix_transpose(matrix):
+    """Return the transpose of a 2D matrix."""
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
