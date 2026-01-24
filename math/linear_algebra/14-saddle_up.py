09b9cfe13b3f8a16e85780fab8bc06f18cff4814
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:50:32 +0200

    14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function np_matmul that performs matrix multiplication."""
+
+
+def np_matmul(mat1, mat2):
+    """Return the matrix product of two numpy.ndarrays."""
+    return np.matmul(mat1, mat2)
