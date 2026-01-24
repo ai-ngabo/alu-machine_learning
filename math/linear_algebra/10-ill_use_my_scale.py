6f91f2d71899df6e838fb61e299f5088e20277cc
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:31:44 +0200

    updates

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -5,8 +5,6 @@ calculates the shape of a numpy.ndarray.
 """
 
 
-import numpy as np
-
 def np_shape(matrix):
     """Return the shape of a numpy.ndarray 
     as a tuple of integers."""
---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Defines a function np_transpose 
+that transposes a numpy.ndarray.
+"""
+
+
+def np_transpose(matrix):
+    """Return the transpose of a numpy.ndarray 
+    as a new array."""
+    return matrix.T
