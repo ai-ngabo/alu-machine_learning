6e3ac0f2aa119aa7072c8ba2382a241766d8024a
ai-ngabo <a.ngabo@alustudent.com>
2026-01-24 23:26:32 +0200

    10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Defines a function np_shape that 
+calculates the shape of a numpy.ndarray.
+"""
+
+
+import numpy as np
+
+def np_shape(matrix):
+    """Return the shape of a numpy.ndarray 
+    as a tuple of integers."""
+    return matrix.shape
