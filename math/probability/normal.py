8087ea2dd3373448068f1602bfe1ef1a36e85f84
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-02-27 11:35:00 +0200

    normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -6,9 +6,6 @@ class Normal:
     """Represents a normal distribution"""
 
     def __init__(self, data=None, mean=0., stddev=1.):
-        """
-        Class constructor
-        """
 
         if data is None:
             if stddev <= 0:
@@ -24,14 +21,23 @@ class Normal:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
-            # Calculate mean
             self.mean = float(sum(data) / len(data))
 
-            # Calculate population standard deviation
             variance = 0
             for x in data:
                 variance += (x - self.mean) ** 2
 
             variance /= len(data)
-
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of x
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score
+        """
+        return z * self.stddev + self.mean
