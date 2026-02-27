3047ab9e37d30a85b1439fd4ced20669cd811400
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-02-27 11:37:48 +0200

    normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -6,38 +6,44 @@ class Normal:
     """Represents a normal distribution"""
 
     def __init__(self, data=None, mean=0., stddev=1.):
-
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
-
             self.mean = float(mean)
             self.stddev = float(stddev)
-
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
             self.mean = float(sum(data) / len(data))
-
-            variance = 0
-            for x in data:
-                variance += (x - self.mean) ** 2
-
-            variance /= len(data)
+            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(variance ** 0.5)
 
     def z_score(self, x):
-        """
-        Calculates the z-score of x
-        """
+        """Calculates the z-score of x"""
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """
-        Calculates the x-value of a given z-score
-        """
+        """Calculates the x-value of a given z-score"""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculates the PDF at x"""
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        exp_part = ((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+        coeff = 1 / (self.stddev * ((2 * pi) ** 0.5))
+        return coeff * (e ** (-exp_part))
+
+    def cdf(self, x):
+        """Calculates the CDF at x"""
+        pi = 3.1415926536
+
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        # Approximate erf(z) using Maclaurin series
+        erf = (z - (z ** 3) / 3 + (z ** 5) / 10 -
+               (z ** 7) / 42 + (z ** 9) / 216)
+        return 0.5 * (1 + erf)
