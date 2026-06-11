22ec4c132b76d10d0a9fe5bcae09dd402b7f53c4
ai-ngabo <a.ngabo@alustudent.com>
2026-06-11 20:53:46 +0200

    file

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -2,7 +2,6 @@
 """Bayesian Optimization module."""
 
 import numpy as np
-from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
@@ -19,25 +18,3 @@ class BayesianOptimization:
 
         self.xsi = xsi
         self.minimize = minimize
-
-    def acquisition(self):
-        """Compute Expected Improvement acquisition function."""
-        mu, sigma2 = self.gp.predict(self.X_s)
-        sigma = np.sqrt(sigma2)
-
-        if self.minimize:
-            f_best = np.min(self.gp.Y)
-            imp = f_best - mu - self.xsi
-        else:
-            f_best = np.max(self.gp.Y)
-            imp = mu - f_best - self.xsi
-
-        sigma = np.where(sigma == 0, 1e-10, sigma)
-        Z = imp / sigma
-
-        EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
-        EI = np.maximum(0, EI)
-
-        X_next = self.X_s[np.argmax(EI)].reshape(-1)
-
-        return X_next, EI
