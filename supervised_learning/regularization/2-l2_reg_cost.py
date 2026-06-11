251ddd305a2e61ee4583d8871f467db3c0e98b85
ai-ngabo <a.ngabo@alustudent.com>
2026-06-11 19:27:57 +0200

    file

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -16,4 +16,8 @@ def l2_reg_cost(cost):
         tensor containing the cost of the network
         accounting for L2 regularization
     """
-    return cost + tf.losses.get_regularization_loss()
+    # Fetch the total scalar regularization loss
+    reg_loss = tf.losses.get_regularization_loss()
+
+    # Add the scalar regularization loss directly to the original cost tensor
+    return cost + reg_loss
