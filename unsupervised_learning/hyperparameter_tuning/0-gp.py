506f02344474e9c8fdcdccddae3fe82fc2aadec5
ai-ngabo <a.ngabo@alustudent.com>
2026-06-11 20:38:16 +0200

    file

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python3
+"""Defines a noiseless 1D Gaussian Process using an RBF kernel."""
+
 import numpy as np
 
+
 class GaussianProcess:
+    """Represents a noiseless 1D Gaussian Process."""
+
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """
-        Initialize a noiseless 1D Gaussian Process
-        """
+        """Initialize Gaussian Process."""
         self.X = X_init
         self.Y = Y_init
         self.l = l
         self.sigma_f = sigma_f
-
-        # Compute initial covariance matrix
         self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
-        """
-        Radial Basis Function (RBF) kernel
-        """
-        # Squared distance between each pair of points
+        """Radial Basis Function kernel."""
         sqdist = (X1 - X2.T) ** 2
-
-        # RBF kernel computation
-        K = (self.sigma_f ** 2) * np.exp(-sqdist / (2 * self.l ** 2))
-
-        return K
+        return (self.sigma_f ** 2) * np.exp(-sqdist / (2 * self.l ** 2))
