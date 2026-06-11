96b07d4c4cf9c4a97213d469ab7e5a505d515a03
ai-ngabo <a.ngabo@alustudent.com>
2026-06-11 20:45:42 +0200

    file

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python3
+"""Bayesian Optimization module."""
+
 import numpy as np
+from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
+    """Performs Bayesian optimization using a Gaussian Process."""
+
     def __init__(self, f, X_init, Y_init, bounds,
                  ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
-        """
-        Bayesian Optimization using a noiseless 1D Gaussian Process
-        """
-
+        """Initialize Bayesian Optimization."""
         self.f = f
-
-        # Gaussian Process model
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
 
-        # Acquisition sampling points (evenly spaced)
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
 
         self.xsi = xsi
         self.minimize = minimize
+
+    def acquisition(self):
+        """Compute Expected Improvement acquisition function."""
+        mu, sigma2 = self.gp.predict(self.X_s)
+        sigma = np.sqrt(sigma2)
+
+        if self.minimize:
+            f_best = np.min(self.gp.Y)
+            imp = f_best - mu - self.xsi
+        else:
+            f_best = np.max(self.gp.Y)
+            imp = mu - f_best - self.xsi
+
+        sigma = np.where(sigma == 0, 1e-10, sigma)
+        Z = imp / sigma
+
+        EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        EI = np.maximum(0, EI)
+
+        X_next = self.X_s[np.argmax(EI)].reshape(-1)
+
+        return X_next, EI
